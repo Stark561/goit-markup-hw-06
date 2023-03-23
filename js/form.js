1882c8b7deab56c4c3f440ad53acb30d6405af71
Stark561 <123174593+Stark561@users.noreply.github.com>
2023-03-23 03:00:35 +0200

    hw

---
+++ js/form.js
@@ -0,0 +1,14 @@
+const formEL = document.querySelector ('[name="contact_form"]');
+
+formEL.addEventListener('submit', event => {
+    event.preventDefault(); 
+    const { user_name, user_phone, user_email, user_comment } = event.currentTarget.elements;
+    if  (user_name.value !== '' && user_phone.value !== '' && user_email.value !== '' && user_comment.value !== '') {
+     console.log('Имя пользователя:', user_name.value);
+     console.log('Номер телефона пользователя:', user_phone.value);
+     console.log('Почта пользователя:', user_email.value);
+     console.log('Комментарий пользователя:', user_comment.value);
+    } else {
+        alert('Введите данные в поля формы');
+    }
+});
